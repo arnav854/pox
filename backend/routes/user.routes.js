f287b88e3126d3478cd989f580451ce33e800d54
Hitesh <hiteshkumarnain@gmail.com>
2025-10-07 22:52:11 +0530

    first

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { login, logout, signup, getMe , sendResetOtp , resetPassword , verifyOtp} from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+export default router;
