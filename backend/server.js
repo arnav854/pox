f729b7c6e4c89f97a2512491dec6928d8ef42d14
[ARNAV] <arnavgoyal1317@gmail.com>
2025-10-07 22:56:40 +0530

    added server and DB removed app

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,33 @@
-// server.js — Entry point of backend
-
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import app from "./app.js";
+import connectToDatabase from "./db/database.js"
+// import authRoutes from "./routes/auth.routes.js";
 
-// Load environment variables from .env
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-const MONGO_URI = process.env.MONGO_DB_URI;
+const app = express();
 
-// Function to connect to MongoDB
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("\x1b[32m%s\x1b[0m", "[INFO] MongoDB Connected"); // green for success
-  } catch (error) {
-    console.error("\x1b[31m%s\x1b[0m", "[ERROR] MongoDB Connection Failed"); // red for error
-    console.error(error);
-    process.exit(1); // stop server if DB fails
-  }
-};
 
-// Start server after DB connection
-const startServer = async () => {
-  await connectDB();
-  app.listen(PORT, () => {
-    console.log("\x1b[36m%s\x1b[0m", `[INFO] Server running on port ${PORT}`); // cyan for server info
-  });
-};
+app.use(cors({
+    origin:  "http://localhost:5173",
+    credentials: true,
+}));
 
-// Start everything
-startServer();
+// JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// parse cookies
+app.use(cookieParser());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+    connectToDatabase();
+});
