7c38494ecb5fb8a108c209e952b54ef288790212
bansalpratham <prathambansal200@gmail.com>
2025-10-08 11:50:24 +0530

    signIn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,53 @@
 import { useState, useEffect } from 'react';
+import SignInForm from './pages/SignIn';
+import SignUpForm from './pages/SignUp';
+import './styles/styles.css';
 
 function App() {
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5001")
-      .then(res => res.json())
-      .then(data => setMsg(data.message))
-      .catch(err => setMsg("Error: " + err));
-  }, []);
-
+  const [type, setType] = useState("signIn");
+  const handleOnClick = text => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container " + (type === "signUp" ? "right-panel-active" : "");
   return (
-    <div>
-      <h1>Frontend + Backend Test</h1>
-      <p>{msg}</p>
+    <div className="App">
+      <h2>Sign in/up Form</h2>
+      <div className={containerClass} id="container">
+        <SignUpForm />
+        <SignInForm />
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleOnClick("signIn")}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button
+                className="ghost "
+                id="signUp"
+                onClick={() => handleOnClick("signUp")}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
